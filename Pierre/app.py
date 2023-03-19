58736f7d3be90a9a016e8dadd9b648d903ec8675
yukino752 <122014810+yukino752@users.noreply.github.com>
2023-03-20 00:56:29 +0100

    Update app.py

--- Pierre/app.py
+++ Pierre/app.py
@@ -1,143 +1,149 @@
-import sqlalchemy
-import pandas
 import dash
-from sqlalchemy import create_engine
-from dash import Dash, dash_table, dcc, html
+import dash_bootstrap_components as dbc
+from dash import html, Dash, dcc, Input, Output
 import mysql.connector
 import pandas as pd
-from dash import Dash, dash_table
 from mysql.connector import errorcode
-from collections import OrderedDict
 
-
-app = Dash(__name__)
-
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = "PAC_Dashboard"
 
-
-def connectDB():
-    try:
-        conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="azerty123",
-            database="test"
-        )
-        print("Connection established")
-
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with the user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
-    else:
-        cursor = conn.cursor()
-    return cursor, conn
-
-
-cur, conn = connectDB()
-
-# CREATION DU FICHIER CSV
-
-num = OrderedDict(
-    [
-        ("N°", ["1", "2", "3", "4", "5", "6", "7"]),
-    ]
-)
-
-
-dfNum = pd.DataFrame(num)
-
-Capteur = OrderedDict([("capteur",
-                        ["Compresseur",
-                         "Condensateur",
-                         "Eau",
-                         "Ventilateur",
-                         "Détendeur",
-                         "Courant",
-                         "Tension"]),
-                       ])
-
-dfCapteur = pd.DataFrame(Capteur)
-
-FusiondfNumdfCapteur = pd.concat([dfNum, dfCapteur], axis=1)
-
-
-def sql_query(query, conn):
-    return pd.read_sql_query(query, conn)
-
-
-query = "SELECT p.HautePression as 'Haute Pression' , p.BassePression as 'Basse Pression' , t.Entree as 'Temperature Entrée' , t.Sortie as 'Temperature Sortie'  FROM pressioncompresseur p INNER JOIN tempcompresseur t ORDER BY idPC DESC LIMIT 1; "
-query2 = "SELECT Entree as 'Temperature Entrée' , Sortie as 'Temperature Sortie'  FROM tempcondenseur ORDER BY idTCD DESC LIMIT 1;"
-query3 = "SELECT Entree as 'Temperature Entrée' , Sortie as 'Temperature Sortie'  FROM tempdetendeur ORDER BY idTD DESC LIMIT 1;"
-query4 = "SELECT Entree as 'Temperature Entrée' , Sortie as 'Temperature Sortie'  FROM tempeau ORDER BY idTE DESC LIMIT 1;"
-query5 = "SELECT Entree as 'Temperature Entrée' , Sortie as 'Temperature Sortie'  FROM tempventilateur ORDER BY idTV DESC LIMIT 1;"
-query6 = "SELECT Volt  FROM Tension ORDER BY idT DESC LIMIT 1;"
-query7 = "SELECT Intensite as 'Intensité'  FROM Courant ORDER BY idC DESC LIMIT 1;"
-
-concatAllSQL = pd.concat(
-    [
-        sql_query(
-            query, conn), sql_query(
-                query2, conn), sql_query(
-                    query3, conn), sql_query(
-                        query4, conn), sql_query(
-                            query5, conn), sql_query(
-                                query6, conn), sql_query(
-                                    query7, conn)])
-
-concatAllSQL = concatAllSQL.reset_index(drop=True)
-
-concatAllData = pd.concat([FusiondfNumdfCapteur, concatAllSQL], axis=1)
-
-concatAllData.to_csv(r'C:\Users\kuro\PycharmProjects\pythonProject\file.csv')
-
-df = pd.read_csv(
-
-    r'C:\Users\kuro\PycharmProjects\pythonProject\file.csv',
-
-    index_col=[0])
-
-
-# Cleanup
-
-conn.commit()
-
-cur.close()
-
-conn.close()
-
-
-app.layout = html.Div([
-
-    dash_table.DataTable(
-
-        id='table',
-
-        columns=[{"name": i, "id": i} for i in df.columns],
-
-        data=df.to_dict('records')
-
+try:
+    conn = mysql.connector.connect(
+        host="host_name",
+        user="username",
+        password="password",
+        database="database_name"
     )
+    print("Connection established")
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with the user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    cursor = conn.cursor()
 
 
+def QueryRequest(cursor, ID):
+    cursor.execute(
+        "SELECT m.valeur FROM  mesure m LEFT JOIN capteur c ON m.fk_id_capteur = c.id where m.fk_id_capteur = " +
+        str(ID) +
+        " ORDER BY m.id DESC LIMIT 1;")
+    res = cursor.fetchone()
+    return res
 
-])
+
+def TupleToInt(QueryRequest):
+    res = QueryRequest
+    res = float(res[0])
+    return res
+
+
+def ValueSchema():
+    values = [
+        html.P(str(TupleToInt(QueryRequest(cursor, 1))) + "bar",
+               style={"z-index": "2", "position": "absolute", "right": "400px", "top": "363px"}),
+        html.P(str(TupleToInt(QueryRequest(cursor, 2))) + "bar",
+               style={"z-index": "2", "position": "absolute", "right": "463px", "top": "363px"}),
+        html.P(str(TupleToInt(QueryRequest(cursor, 3))) + "°C",
+               style={"z-index": "2", "position": "absolute", "right": "460px", "top": "249px"}),
+        html.P(str(TupleToInt(QueryRequest(cursor, 4))) + "°C",
+               style={"z-index": "2", "position": "absolute", "right": "403px", "top": "220px"}),
+        html.P(str(TupleToInt(QueryRequest(cursor, 5))) + "°C",
+               style={"z-index": "2", "position": "absolute", "right": "130px", "top": "130px"}),
+        html.P(str(TupleToInt(QueryRequest(cursor, 6))) + "°C",
+               style={"z-index": "2", "position": "absolute", "right": "275px", "top": "140px"}),
+        html.P(str(TupleToInt(QueryRequest(cursor, 7))) + "°C",
+               style={"z-index": "2", "position": "absolute", "right": "397px", "top": "530px"}),
+        html.P(str(TupleToInt(QueryRequest(cursor, 8))) + "°C",
+               style={"z-index": "2", "position": "absolute", "right": "475px", "top": "530px"}),
+        html.P(str(TupleToInt(QueryRequest(cursor, 9))) + "°C",
+               style={"z-index": "2", "position": "absolute", "right": "560px", "top": "313px"}),
+        html.P(str(TupleToInt(QueryRequest(cursor, 10))) + "°C",
+               style={"z-index": "2", "position": "absolute", "right": "112px", "top": "470px"}),
+    ]
+
+    return values
+
+
+def generateTable():
+    table_header = [html.Thead(html.Tr([html.Th("Désignation"), html.Th(
+        "Point de mesures"), html.Th("Valeur"), html.Th("Unité")]))]
+
+    row8 = html.Tr([html.Td("Sortie du detenteur (T6)"), html.Td(
+        "6"), html.Td(QueryRequest(cursor, 8)), html.Td("°C")])
+    row1 = html.Tr([html.Td("Haute Pression (HP)"), html.Td(
+        "1"), html.Td(QueryRequest(cursor, 1)), html.Td("Bar")])
+    row2 = html.Tr([html.Td("Basse Pression (BP)"), html.Td(
+        "2"), html.Td(QueryRequest(cursor, 2)), html.Td("Bar")])
+    row3 = html.Tr([html.Td("Entrée du compresseur (T1)"), html.Td(
+        "1"), html.Td(QueryRequest(cursor, 3)), html.Td("°C")])
+    row4 = html.Tr([html.Td("Sortie du compresseur (T2)"), html.Td(
+        "2"), html.Td(QueryRequest(cursor, 4)), html.Td("°C")])
+    row5 = html.Tr([html.Td("Entrée du condenseur (T3)"), html.Td(
+        "3"), html.Td(QueryRequest(cursor, 5)), html.Td("°C")])
+    row6 = html.Tr([html.Td("Sortie du condenseur (T4)"), html.Td(
+        "4"), html.Td(QueryRequest(cursor, 6)), html.Td("°C")])
+    row7 = html.Tr([html.Td("Entrée du detenteur (T5)"), html.Td(
+        "5"), html.Td(QueryRequest(cursor, 7)), html.Td("°C")])
+    row9 = html.Tr([html.Td("Sortie d'évaporateur (T7)"), html.Td(
+        "7"), html.Td(QueryRequest(cursor, 9)), html.Td("°C")])
+    row10 = html.Tr([html.Td("Bac d'eau (T8)"), html.Td(
+        "8"), html.Td(QueryRequest(cursor, 10)), html.Td("°C")])
+
+    table_body = [html.Tbody(
+        [row1, row2, row3, row4, row5, row6, row7, row8, row9, row10])]
+    return dbc.Table(
+        table_header +
+        table_body,
+        striped=True,
+        hover=True,
+        responsive=True,
+        style={
+            "textAlign": "center",
+            "width": "95%",
+            "bottom": "0",
+            "top": "0"})
+
+
+body = dbc.Container([dbc.Row([dbc.Col([html.H1("PAC Dashboard",
+                                                style={"color": "red",
+                                                       "textAlign": "center"}),
+                                        ],
+                                       ),
+                               ]),
+                      dbc.Row([dbc.Col([html.Div(generateTable(),
+                               id="table-data",
+                               style={"margin-top": "35px",
+                                       "z-index": "2"})],
+                                       ),
+                               dbc.Col([html.Img(src=app.get_asset_url('schema.png'),
+                                                 style={"position": "absolute",
+                                                        "top": "120px",
+                                                        "right": "120px",
+                                                        "z-index": "1"}),
+                                        html.Div(ValueSchema(),
+                                                 style={"color": "blue",
+                                                        "font-size": "12px"})])]),
+                      ])
+app.layout = html.Div([body, dcc.Interval(
+    id='interval-component', interval=1 * 10000, n_intervals=0), ])
+
+
+@app.callback(Output('table-data', 'children'),
+              [Input('interval-component', 'n_intervals')])
+def update_table(interval):
+    return generateTable()
 
 
 if __name__ == '__main__':
-
     app.run_server(debug=True)
 
-
 """
-
 engine = create_engine("mysql://pierre:azerty@localhost/test")
-
 source_connection = engine.connect()
-
 df = pandas.read_sql("SELECT * FROM inventory", con=engine)
-
 """
