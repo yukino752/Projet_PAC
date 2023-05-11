2c110abb3f3e2c1b642201396f7b967f881208ed
ZoxSky <waroux.a@stjolorient.fr>
2023-05-11 16:49:29 +0200

    AméliorationV5

---
+++ Clement/RecupValeurV5.py
@@ -0,0 +1,190 @@
+import time
+import serial
+import signal
+import re
+from CreateDatabase import *
+from Insert_Database import *
+from Select_Database import *
+
+connectDB()
+Create_Table()
+
+def signal_handler(sig, frame):
+    print('Exiting...')
+    arduino.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+def read_capteur(liaison_serie, point_de_mesure):
+    assert isinstance(point_de_mesure, str), "point_de_mesure est du mauvais type : " + type(point_de_mesure)
+    print("DEBUG 0 : point de mesure = " + point_de_mesure)
+    #answer = str(arduino.readline())
+    #print("réponse : " + answer)
+    message = liaison_serie.readline().decode('utf-8', errors="replace")
+
+    assert message != "", "le message recu est vide" + message
+
+    print("DEBUG 1 : message recu sur la ligne série = "  + message)
+
+    dispositif_switcher = {
+        "Compresseur"  : Compresseur,
+        "Detendeur"  : Detendeur,
+        "Condenseur"  : Condenseur,
+        "Evaporateur"  : Evaporateur,
+        "Eau" : Eau,
+        "Pression"  : Compresseur
+    }
+
+    match = re.search(r'[0-9]+(\.[0-9]+)?\r?\n?$', message)
+    assert match is not None, "valeur de mesure invalide"
+    valeur_mesure = float(match.group(0))
+
+    clef = message
+    for motif in [r'[0-9]+(\.[0-9]+)?\r?\n$', '^(temperature|basse|haute)', 'Entree|Sortie']:
+        clef = re.sub(motif, '', clef)
+
+    assert clef in dispositif_switcher, "clef invalide, message reçu incomplet ?"
+    print("DEBUG 2 : clef = "+ clef +", answer = "+ message +", valeur_mesure = "+ str(valeur_mesure))
+
+    dispositif_func = dispositif_switcher[clef]
+
+    validite_min, validite_max = Select_validite(point_de_mesure)
+    print(validite_min)
+    id_capteur = Select_id_Capteur(point_de_mesure)
+    id_releve = 1
+    if valeur_mesure>validite_min and valeur_mesure<validite_max:
+        dispositif_func(valeur_mesure, id_capteur, id_releve)
+    else:
+        dispositif_func(None, id_capteur, id_releve)
+               
+    """    
+    if 'temperatureEntreeDetendeur' in answer:
+        print(answer)
+        temperatureEntreeDetendeur = answer.split("temperatureEntreeDetendeur")
+        print(temperatureEntreeDetendeur[1])
+        validite_min, validite_max = Select_id_Capteur('temperatureEntreeDetendeur')
+        print(validite_min)
+        id_capteur = Select_id_Capteur('TemperatureEntreeDentendeur')
+        id_releve = 1
+        if temperatureEntreeDetendeur[1]>validite_min and temperatureEntreeDetendeur[1]<validite_max:
+            Detendeur(temperatureEntreeDetendeur[1], id_capteur, id_releve)
+        else:
+            Detendeur(None, id_capteur, id_releve)
+
+    elif 'temperatureSortieDetendeur' in answer:
+        print(answer)
+        temperatureSortieDetendeur = answer.split("temperatureSortieDetendeur")
+        print(temperatureSortieDetendeur[1])
+        validite_min, validite_max = Select_id_Capteur('temperatureSortieDetendeur')
+        id_capteur = Select_id_Capteur('TemperatureSortieDentendeur')
+        id_releve = 1
+        if temperatureSortieDetendeur[1]>validite_min and temperatureSortieDetendeur[1]<validite_max:
+            Detendeur(temperatureSortieDetendeur[1], id_capteur, id_releve)
+        else:
+            Detendeur(None, id_capteur, id_releve)
+
+    elif 'temperatureEntreeCompresseur' in answer:
+        print(answer)
+        temperatureEntreeCompresseur = answer.split("temperatureEntreeCompresseur")
+        print(temperatureEntreeCompresseur[1])
+        validite_min, validite_max = Select_id_Capteur('temperatureEntreeCompresseur')
+        id_capteur = Select_id_Capteur('TemperatureEntreeCompresseur')
+        id_releve = 1
+        if temperatureEntreeCompresseur[1]>validite_min and temperatureEntreeCompresseur[1]<validite_max:
+            Compresseur(temperatureEntreeCompresseur[1], id_capteur, id_releve)
+        else:
+            Compresseur(None, id_capteur, id_releve)
+
+    elif 'temperatureSortieCompresseur' in answer:
+        print(answer)
+        temperatureSortieCompresseur = answer.split("temperatureSortieCompresseur")
+        print(temperatureSortieCompresseur[1])
+        validite_min, validite_max = Select_id_Capteur('temperatureSortieCompresseur')
+        id_capteur = Select_id_Capteur('TemperatureSortieCompresseur')
+        id_releve = 1
+        if temperatureSortieCompresseur[1]>validite_min and temperatureSortieCompresseur[1]<validite_max:
+            Compresseur(temperatureSortieCompresseur[1], id_capteur, id_releve)
+        else:
+            Compresseur(None, id_capteur, id_releve)
+
+    elif 'temperatureEntreeCondenseur' in answer:
+        print(answer)
+        temperatureEntreeCondenseur = answer.split("temperatureEntreeCondenseur")
+        print(temperatureEntreeCondenseur[1])
+        print("Debug : " + str(type(Select_id_Capteur('temperatureEntreeCondenseur'))))
+        validite_min, validite_max = Select_id_Capteur('temperatureEntreeCondenseur')
+        id_capteur = Select_id_Capteur('TemperatureEntreeCondenseur')
+        id_releve = 1
+        if temperatureEntreeCondenseur[1]>validite_min and temperatureEntreeCondenseur[1]<validite_max:
+            Condenseur(temperatureEntreeCondenseur[1], id_capteur, id_releve)
+        else:
+            Condenseur(None, id_capteur, id_releve)
+
+    elif 'temperatureSortieCondenseur' in answer:
+        print(answer)
+        temperatureSortieCondenseur = answer.split("temperatureSortieCondenseur")
+        print(temperatureSortieCondenseur[1])
+        validite_min, validite_max = Select_id_Capteur('temperatureSortieCondenseur')
+        id_capteur = Select_id_Capteur('TemperatureSortieCondenseur')
+        id_releve = 1
+        if temperatureSortieCondenseur[1]>validite_min and temperatureSortieCondenseur[1]<validite_max:
+            Condenseur(temperatureSortieCondenseur[1], id_capteur, id_releve)
+        else:
+            Condenseur(None, id_capteur, id_releve)
+
+    elif 'temperatureSortieEvaporateur' in answer:
+        print(answer)
+        temperatureSortieEvaporateur = answer.split("temperatureSortieEvaporateur")
+        print(temperatureSortieEvaporateur[1])
+        validite_min, validite_max = Select_id_Capteur('temperatureSortieEvaporateur')
+        id_capteur = Select_id_Capteur('TemperatureSortieEvaporateur')
+        id_releve = 1
+        if temperatureSortieEvaporateur[1]>validite_min and temperatureSortieEvaporateur[1]<validite_max:
+            Evaporateur(temperatureSortieEvaporateur[1], id_capteur, id_releve)
+        else:
+            Evaporateur(None, id_capteur, id_releve)
+        
+    elif 'temperatureEau' in answer:
+        print(answer)
+        temperatureEau = answer.split("temperatureEau")
+        print(temperatureEau[1])
+        validite_min, validite_max = Select_id_Capteur('temperatureEau')
+        id_capteur = Select_id_Capteur('TemperatureEau')
+        id_releve = 1
+        if temperatureEau[1]>validite_min and temperatureEau[1]<validite_max:
+            Eau(temperatureEau[1], id_capteur, id_releve)
+        else:
+            Eau(None, id_capteur, id_releve)
+        
+    elif 'hautePression' in answer:
+        print(answer)
+        hautePression = answer.split("hautePression")
+        print(hautePression[1])
+        validite_min, validite_max = Select_id_Capteur('hautePression')
+        id_capteur = Select_id_Capteur('hautePression')
+        id_releve = 1
+        if hautePression[1]>validite_min and hautePression[1]<validite_max:
+            Compresseur(hautePression[1], id_capteur, id_releve)
+        else:
+            Compresseur(None, id_capteur, id_releve)
+        
+    elif 'bassePression' in answer:
+        print(answer)
+        bassePression = answer.split("bassePression")
+        print(bassePression[1])
+        validite_min, validite_max = Select_id_Capteur('bassePression')
+        id_capteur = Select_id_Capteur('bassePression')
+        id_releve = 1
+        if bassePression[1]>validite_min and bassePression[1]<validite_max:
+            Compresseur(bassePression[1], id_capteur, id_releve)
+        else:
+            Compresseur(None, id_capteur, id_releve)
+	 """ 
+
+
+
+if __name__ == '__main__':
+    print('Running. Press CTRL-C to exit.')
+   # with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
+   #     arduino.close()
