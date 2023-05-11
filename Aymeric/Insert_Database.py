ad59433ed4d7c0c861c12d8ce4b629e57ab62063
ZoxSky <waroux.a@stjolorient.fr>
2023-05-11 11:37:46 +0200

    AméliorationV2

--- Aymeric/Insert_Database.py
+++ Aymeric/Insert_Database.py
@@ -37,11 +37,40 @@ def Detendeur (Valeur, id_capteur, id_releve):
     cursor.execute ("INSERT INTO mesure (fk_id_releve, fk_id_capteur, valeur) VALUES (%s, %s, %s);", (id_releve, id_capteur, Valeur)) # Exécution de la requête SQL pour ajouter une mesure
     conn.commit() # Validation des changements dans la base de données
 
-def Evaporateur (Valeur, id_capteur, id_releve):
+def Evaporateur(Valeur, id_capteur, id_releve):
     """
     Cette fonction ajoute une mesure de la valeur d'un capteur associé à un relevé dans la table 'mesure' de la base de données.
     :param Valeur: la valeur du capteur à ajouter
-    :param id_capteur:
+    :param id_capteur: l'ID du capteur
+    :param id_releve: l'ID du relevé associé
+    """
+    conn = connectDB()  # Connexion à la base de données
+    cursor = conn.cursor()  # Création d'un curseur pour exécuter les requêtes SQL
+    cursor.execute("INSERT INTO mesure (fk_id_releve, fk_id_capteur, valeur) VALUES (%s, %s, %s);",
+                   (id_releve, id_capteur, Valeur))  # Exécution de la requête SQL pour ajouter une mesure
+    conn.commit()  # Validation des changements dans la base de données
+
+def Eau(Valeur, id_capteur, id_releve):
+    """
+    Cette fonction ajoute une mesure de la valeur d'un capteur associé à un relevé dans la table 'mesure' de la base de données.
+    :param Valeur: la valeur du capteur à ajouter
+    :param id_capteur: l'ID du capteur
+    :param id_releve: l'ID du relevé associé
+    """
+    conn = connectDB()  # Connexion à la base de données
+    cursor = conn.cursor()  # Création d'un curseur pour exécuter les requêtes SQL
+    cursor.execute("INSERT INTO mesure (fk_id_releve, fk_id_capteur, valeur) VALUES (%s, %s, %s);",
+                   (id_releve, id_capteur, Valeur))  # Exécution de la requête SQL pour ajouter une mesure
+    conn.commit()  # Validation des changements dans la base de données
+
+def Releve():
+    """
+    Cette fonction ajoute un relevé avec la date actuelle dans la table 'releve' de la base de données.
+    """
+    conn = connectDB()  # Connexion à la base de données
+    cursor = conn.cursor()  # Création d'un curseur pour exécuter les requêtes SQL
+    cursor.execute("INSERT INTO releve (date) VALUES (CURRENT_TIMESTAMP);")  # Exécution de la requête SQL pour ajouter un relevé
+    conn.commit()  # Validation des changements dans la base de données
 
 
 #########################################################################################################
